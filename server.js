e1ff2ce9621d1d40740eb42135dac1372fd2e8d1
hooman734 <hesamyan@outlook.com>
2020-12-08 22:25:43 +0400

    resolve server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
-app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
